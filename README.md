7148963b2bea80033c6db05035e0cd19a213839b
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-06-09 12:08:25 +0200

    [Add] Migration wines table + README

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # La vineria
 Sito Catalogo dei migliori Vini.
+
+## Steps
+- Aggiunta la migration per la tabella dei vini.
+
+---
+
+## Suggest!
+
+**Per lavorare sul progetto** 
+
+1.  Scaricare tutte le dipendenze npm e composer con i comandi : 
+```
+npm i
+```
+```
+composer install
+```
+2. Duplicare il file .env e generare la propria chiave con il comando :
+```
+php artisan key:generate
+```
+3. Creare il proprio db in locale
+4. Collegare il db nel file `.env`
+5. Lanciare il comando 
+```
+php artisan migrate
+```
+
+!!! Dare sempre ragione al boss
+
---
+++ database/migrations/2023_06_09_095117_create_wines_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wines', function (Blueprint $table) {
+            $table->id();
+            $table->string('winery' , 50);
+            $table->string('wine');
+            $table->float('rating_average', 2 ,1);
+            $table->string('rating_review', 50);
+            $table->string('location');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wines');
+    }
+};
